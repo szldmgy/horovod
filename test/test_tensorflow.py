b1467c11f3975835f66fcc60b965d9889d963ca1
Travis Addair <taddair@uber.com>
2018-07-26 16:41:15 -0700

    Changed compressor to set the dtype on initialization

--- test/test_tensorflow.py
+++ test/test_tensorflow.py
@@ -619,7 +619,7 @@ class MPITests(tf.test.TestCase):
             for dtype in valid_dtypes:
                 tensor = tf.ones(tensor_size, dtype=dtype)
 
-                compressor = compression.get_compressor()
+                compressor = compression.get_compressor(dtype)
                 tensor_compressed = compressor.compress(tensor)
                 self.assertEqual(tensor_compressed.dtype, tf.float16)
 
@@ -634,7 +634,7 @@ class MPITests(tf.test.TestCase):
             for dtype in invalid_dtypes:
                 tensor = tf.ones(tensor_size, dtype=dtype)
 
-                compressor = compression.get_compressor()
+                compressor = compression.get_compressor(dtype)
                 tensor_compressed = compressor.compress(tensor)
                 self.assertEqual(tensor_compressed.dtype, dtype)
 
